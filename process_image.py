34dc15d29512a45b816ec3b6fca0a0a24bf642fd
sudarshan <sudarshanreddy75@gmail.com>
2022-03-16 22:46:35 -0700

    Testing response queue

--- process_image.py
+++ process_image.py
@@ -81,22 +81,32 @@ class ProcessImage():
             f.write(file_name + ":" + content)
         # s3_client.upload_file(file_path, self.s3_output_bucket_name, file_name.split(".")[0] + ".txt")
         # print("File uploaded succesfully!!")
-        self.upload_result_to_sqs_response(file_path)
+        self.upload_result_to_sqs_response(file_path, s3_client)
         
-    def upload_result_to_sqs_response(self, file_path):
-        
+    def upload_result_to_sqs_response(self, file_path, s3_client):
         with open(file_path, "r") as f:
             content = f.readline()
             key, value = content.split(":")
             message = {key : value}
             response = self.sqs_client.send_message(QueueUrl=self.sqs_response_queue, MessageBody=json.dumps(message))
             print("Message Upload to Response Queue", response)
-            
+            self.upload_to_sqs_from_s3(s3_client)
+    
+    def upload_to_sqs_from_s3(self, s3_client):
+        s3_client = boto3.client("s3")
+        queue = self.sqs_service.get_queue_by_name(QueueName=self.sqs_response_queue)
+        messages = queue.receive_messages(MaxNumberOfMessages=1, WaitTimeSeconds=20)
+        if messages:
+            for m in messages:
+                content = json.loads(m.body)
+                s3_client.put_object(Body=list(content.values())[0], Bucket=self.s3_output_bucket_name, Key=list(content.keys())[0].split(".")[0]+".txt")
+                
 if __name__=="__main__":
     process_image = ProcessImage()
-    while True:
-        content = process_image.fetch_image_from_sqs()
-        if content[0] == "process":
-            process_image.send_image_for_processing(content)
-        else:
-            break
+    # while True:
+    #     content = process_image.fetch_image_from_sqs()
+    #     if content[0] == "process":
+    #         process_image.send_image_for_processing(content)
+    #     else:
+    #         break
+    process_image.upload_to_sqs_from_s3("xxx")
